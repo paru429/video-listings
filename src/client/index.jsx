e55dfca01fbf8f70f5975e50b36df8f7e2a005a0
Parvathi <parvathivallinayagam96@gmail.com>
2021-04-19 22:54:29 +0530

    Add redux devtools extension

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,10 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./javascripts/Components/App";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import reducers from './javascripts/reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
